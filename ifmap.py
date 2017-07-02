d2938690da0d72fa90746e63efc3276a65dc1b23
Andrew Plotkin <zarf@Zarfslab.local>
2017-07-01 22:11:46 -0400

    Parse the paramfile (lib/index)

--- ifmap.py
+++ ifmap.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import sys
+import os.path
 import optparse
 
 popt = optparse.OptionParser(usage='ifmap.py')
@@ -8,10 +10,13 @@ popt.add_option('--index',
                 action='store', dest='indexpath',
                 help='pathname of Master-Index')
 popt.add_option('--src',
-                action='store', dest='libpath',
+                action='store', dest='libdir',
                 help='pathname of directory containing template files')
+popt.add_option('--tree',
+                action='store', dest='treedir',
+                help='pathname of directory containing archive files')
 popt.add_option('--dest',
-                action='store', dest='destpath',
+                action='store', dest='destdir',
                 help='pathname of directory to write index files')
 
 popt.add_option('--xml',
@@ -24,4 +29,34 @@ popt.add_option('-v', '--verbose',
                 action='store_true', dest='verbose',
                 help='print verbose output')
 
+class ParamFile:
+    def __init__(self, filename):
+        self.filename = filename
+        self.map = {}
+        self.body = ''
+        
+        fl = open(filename, encoding='utf-8')
+        while True:
+            ln = fl.readline()
+            if not ln:
+                break
+            ln = ln.strip()
+            if not ln:
+                break
+            key, dummy, val = ln.partition(':')
+            if not dummy:
+                print('Problem: no colon in header line: %d' % (ln,))
+                continue
+            self.map[key.strip()] = val.strip()
+
+        self.body = fl.read()
+        fl.close()
+
 (opts, args) = popt.parse_args()
+
+if not opts.libdir:
+    raise Exception('--src argument required')
+if not opts.destdir:
+    raise Exception('--dest argument required')
+
+plan = ParamFile(os.path.join(opts.libdir, 'index'))
