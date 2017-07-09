3035c8d84323cd9b479b657b1cb5621c6a98c92c
Andrew Plotkin <zarf@Zarfslab.local>
2017-07-09 00:37:19 -0400

    Test escape_htmldesc_string.

--- tests.py
+++ tests.py
@@ -1,6 +1,9 @@
 import unittest
 
-from ifmap import escape_html_string
+# To run:
+#   python3 tests.py
+
+from ifmap import escape_html_string, escape_htmldesc_string
 
 class TestEscapeFunctions(unittest.TestCase):
     def test_escape_html_string(self):
@@ -9,7 +12,16 @@ class TestEscapeFunctions(unittest.TestCase):
         self.assertEqual(escape_html_string('w x\ny\tz'), 'w x\ny\tz')
         self.assertEqual(escape_html_string('x\x01y'), 'x&#x1;y')
         self.assertEqual(escape_html_string('© αβγδε “”'), '&#xA9; &#x3B1;&#x3B2;&#x3B3;&#x3B4;&#x3B5; &#x201C;&#x201D;')
-        
+
+    def test_escape_htmldesc_string(self):
+        self.assertEqual(escape_htmldesc_string('foo'), 'foo')
+        self.assertEqual(escape_htmldesc_string('foo<i>'), 'foo&lt;i&gt;')
+        self.assertEqual(escape_htmldesc_string('w x\ny\tz'), 'w x\ny\tz')
+        self.assertEqual(escape_htmldesc_string('<http://foo>'), '<a href="http://foo">http://foo</a>')
+        self.assertEqual(escape_htmldesc_string('<http://foo> <https://bar/b> <x>'), '<a href="http://foo">http://foo</a> <a href="https://bar/b">https://bar/b</a> &lt;x&gt;')
+        ### escape_htmldesc_string currently does not escape & or &...;
+        ### sequences. Add tests when it does.
+
 if __name__ == '__main__':
     unittest.main()
     
