0b664ffd97aaf9df6c5d25fbbd3ee0dd4742b298
Andrew Plotkin <zarf@Zarfslab.local>
2017-07-09 01:12:31 -0400

    Simple template test.

--- tests.py
+++ tests.py
@@ -1,13 +1,17 @@
-import unittest
+#!/usr/bin/env python3
 
 # To run:
 #   python3 tests.py
+
+import unittest
+import io
 
 from ifmap import escape_html_string, escape_htmldesc_string, escape_url_string
 from ifmap import is_string_nonwhite
 from ifmap import Template
 
 class TestEscapeFunctions(unittest.TestCase):
+    
     def test_escape_html_string(self):
         self.assertEqual(escape_html_string('foo'), 'foo')
         self.assertEqual(escape_html_string('foo<i>&'), 'foo&lt;i&gt;&amp;')
@@ -38,6 +42,25 @@ class TestEscapeFunctions(unittest.TestCase):
         self.assertIs(is_string_nonwhite('x      \n'), True)
         self.assertIs(is_string_nonwhite('\n      x'), True)
         
+class TestSubstitutions(unittest.TestCase):
+
+    def substitute(self, body, map):
+        fl = io.StringIO()
+        Template.substitute(body, map, outfl=fl)
+        res = fl.getvalue()
+        fl.close()
+        return res
+
+    def test_basic(self):
+        self.assertEqual(self.substitute('foo', {}), 'foo')
+        self.assertEqual(self.substitute('One\nαβγδε\n<&>“”\n', {}), 'One\nαβγδε\n<&>“”\n')
+        self.assertEqual(self.substitute('escape {{}!', {}), 'escape {!')
+        self.assertEqual(self.substitute('foo={bar}', { 'bar':'xxx' }), 'foo=xxx')
+        self.assertEqual(self.substitute('foo={bar}', { 'bar':123 }), 'foo=123')
+        self.assertEqual(self.substitute('foo={bar}', { 'bar':True }), 'foo=True')
+        self.assertEqual(self.substitute('foo={bar}', { 'bar':False }), 'foo=False')
+        self.assertEqual(self.substitute('foo={bar}', { 'bar':'' }), 'foo=')
+    
 if __name__ == '__main__':
     unittest.main()
     
