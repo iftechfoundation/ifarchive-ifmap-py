79231d143658373f60a7c42fa5eb509a0fdc5a87
Andrew Plotkin <zarf@Zarfslab.local>
2017-07-09 00:43:35 -0400

    escape_url_string tests.

--- tests.py
+++ tests.py
@@ -3,7 +3,7 @@ import unittest
 # To run:
 #   python3 tests.py
 
-from ifmap import escape_html_string, escape_htmldesc_string
+from ifmap import escape_html_string, escape_htmldesc_string, escape_url_string
 
 class TestEscapeFunctions(unittest.TestCase):
     def test_escape_html_string(self):
@@ -18,10 +18,15 @@ class TestEscapeFunctions(unittest.TestCase):
         self.assertEqual(escape_htmldesc_string('foo<i>'), 'foo&lt;i&gt;')
         self.assertEqual(escape_htmldesc_string('w x\ny\tz'), 'w x\ny\tz')
         self.assertEqual(escape_htmldesc_string('<http://foo>'), '<a href="http://foo">http://foo</a>')
+        self.assertEqual(escape_htmldesc_string('<http://foo'), '&lt;http://foo')
         self.assertEqual(escape_htmldesc_string('<http://foo> <https://bar/b> <x>'), '<a href="http://foo">http://foo</a> <a href="https://bar/b">https://bar/b</a> &lt;x&gt;')
         ### escape_htmldesc_string currently does not escape & or &...;
         ### sequences. Add tests when it does.
 
+    def test_escape_url_string(self):
+        self.assertEqual(escape_url_string('foo'), 'foo')
+        self.assertEqual(escape_url_string('http://foo/bar?x&y=z%'), 'http://foo/bar%3Fx%26y%3Dz%25')
+        
 if __name__ == '__main__':
     unittest.main()
     
