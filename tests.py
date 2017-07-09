61786b0e9236a82724e3585393e84493a1c1749a
Andrew Plotkin <zarf@Zarfslab.local>
2017-07-09 00:50:38 -0400

    Test is_string_nonwhite.

--- tests.py
+++ tests.py
@@ -4,6 +4,8 @@ import unittest
 #   python3 tests.py
 
 from ifmap import escape_html_string, escape_htmldesc_string, escape_url_string
+from ifmap import is_string_nonwhite
+from ifmap import Template
 
 class TestEscapeFunctions(unittest.TestCase):
     def test_escape_html_string(self):
@@ -27,6 +29,14 @@ class TestEscapeFunctions(unittest.TestCase):
     def test_escape_url_string(self):
         self.assertEqual(escape_url_string('foo'), 'foo')
         self.assertEqual(escape_url_string('http://foo/bar?x&y=z%'), 'http://foo/bar%3Fx%26y%3Dz%25')
+
+    def test_is_string_nonwhite(self):
+        self.assertIs(is_string_nonwhite(''), False)
+        self.assertIs(is_string_nonwhite('   '), False)
+        self.assertIs(is_string_nonwhite('\n  \t  \n'), False)
+        self.assertIs(is_string_nonwhite('x'), True)
+        self.assertIs(is_string_nonwhite('x      \n'), True)
+        self.assertIs(is_string_nonwhite('\n      x'), True)
         
 if __name__ == '__main__':
     unittest.main()
