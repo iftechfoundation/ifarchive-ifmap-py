a45dc98c26e9e44b5f15851c5b54fb197f40e740
Andrew Plotkin <zarf@ZarfLent.local>
2018-12-28 19:08:57 -0500

    Set timestamps on test data.

---
+++ testdata/set-timestamps.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+# This sets the timestamps of all the files under testdata/if-archive.
+# This lets us run ifmap.py on the test data with predictable results.
+
+import sys, os
+import time
+
+common_timestamp = 1540000000  # 20 Oct 2018
+
+timestamps = {
+    'file-1.txt': 1539500000,
+    'file-2.txt': 1539600000,
+    'file-3.txt': 1539700000,
+}
+
+for root, dirs, files in os.walk('testdata/if-archive'):
+    for file in files:
+        val = timestamps.get(file, common_timestamp)
+        path = os.path.join(root, file)
+        if os.path.islink(path):
+            continue
+        os.utime(path, times=(val, val))
+        timestr = time.strftime('%H:%M:%S %d-%b-%Y', time.gmtime(val))
+        print('%s: %s' % (path, timestr))
+        
