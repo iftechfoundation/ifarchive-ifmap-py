77e6f6630cb27dbd422eb4ef445b2d40fdeb7b3b
Andrew Plotkin <zarf@ZarfLent.local>
2024-02-03 21:50:14 -0500

    Update make-master-index.py to not rely on ls-lR.

--- make-master-index.py
+++ make-master-index.py
@@ -2,58 +2,52 @@
 
 """
 This constructs the Master-Index file (the plain text one, not the XML).
-It works by reading the ls-lR listing, extracting all the Index files,
+It works by walking the directory tree, extracting all the Index files,
 and concatenating them together. It adds a # directory header at the top
 of each Index file, and increases the # header depth (of files, etc) by
 one.
 
-Usage: make-master-index.py ls-lR [ htdocs ]
+Usage: make-master-index.py [ htdocs ]
 
-The second argument is optional, and defaults to /var/ifarchive/htdocs.
+The argument is optional, and defaults to /var/ifarchive/htdocs.
+
+(This used to read the ls-lR file, but not any more.)
 
 In normal Archive operation:
 
-    cd /var/ifarchive/htdocs
-    ls -lRn if-archive > if-archive/ls-lR
-    /var/ifarchive/bin/make-master-index.py if-archive/ls-lR > if-archive/Master-Index
+    /var/ifarchive/bin/make-master-index.py > if-archive/Master-Index
 
 """
 
 import sys
+import os, os.path
 import re
 
-lspath = sys.argv[1]
 rootdir = '/var/ifarchive/htdocs'
-if len(sys.argv) >= 3:
-    rootdir = sys.argv[2]
+if len(sys.argv) >= 2:
+    rootdir = sys.argv[1]
 
+rootarchdir = os.path.join(rootdir, 'if-archive')
+    
 dirre = re.compile('^if-archive.*:$')
 
-currentdir = 'if-archive'
+for (dirpath, dirnames, filenames) in os.walk(rootarchdir):
+    if 'Index' in filenames:
+        currentdir = os.path.relpath(dirpath, start=rootdir)
+        basename = (currentdir + ':')
+        print()
+        print('# ' + basename)
+        filename = (rootdir + '/' + currentdir + '/Index')
+        fl = open(filename, 'r', encoding='utf-8')
+        for subln in fl.readlines():
+            if subln.startswith('#'):
+                subln = '#'+subln
+            print(subln, end='')
+        fl.close()
+        str = None
+        print()
+        print('------------------------------------------------------')
+    
+    # Ensure that we visit in Unicode sort order (not case-folded).
+    dirnames.sort()
 
-lsfile = open(lspath, encoding='utf-8')
-
-for line in lsfile.readlines():
-    res = dirre.match(line)
-    if (res != None):
-        line = line.strip()
-        line = line[ : -1]
-        currentdir = line
-    else:
-        line = line.strip()
-        if (line[-6 : ] == ' Index' and currentdir != None):
-            basename = (currentdir + ':')
-            print()
-            print('# ' + basename)
-            filename = (rootdir + '/' + currentdir + '/Index')
-            fl = open(filename, 'r', encoding='utf-8')
-            for subln in fl.readlines():
-                if subln.startswith('#'):
-                    subln = '#'+subln
-                print(subln, end='')
-            fl.close()
-            str = None
-            print()
-            print('------------------------------------------------------')
-            
-lsfile.close()
