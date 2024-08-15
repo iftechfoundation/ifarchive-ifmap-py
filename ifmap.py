74fa0726750ff87deb6d51e01c9e76eacccd2f0a
Andrew Plotkin <zarf@ZarfLent.local>
2024-08-15 18:17:07 -0400

    Comment.

--- ifmap.py
+++ ifmap.py
@@ -234,7 +234,7 @@ filehash_pattern = re.compile('([^a-zA-Z0-9_.,;:()@/-])')
 filehash_escaper = lambda match: '=%02X=' % (ord(match.group(1)),)
 def filehash(val):
     """Escape a filename in a way that can appear in a DOM id or URL
-    fragment (dir#filename).
+    fragment (dir#filename). This also works on full pathnames.
     (Nothing in the system needs to reverse this mapping, but it
     should be unique.)
     This is a bit arbitrary. Almost any non-whitspace character is legal
